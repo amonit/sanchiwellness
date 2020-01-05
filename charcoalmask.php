2140a1b7cb988651d558cd5a34ec1121bc15bd59
udk-in <uditsmailbox@gmail.com>
2020-01-05 21:07:58 +0530

    Charcoal mask page added

---
+++ charcoalmask.php
@@ -0,0 +1,31 @@
+<?php
+include_once 'head.php';
+?>
+
+<div class="container">
+	<div class="row">
+		<h2>Charcoal Mask</h2>
+				
+		<h3>What are the benefits of a charcoal mask?</h3>
+		<p>You’ll find activated charcoal in many personal care products, including cleansers, lotions, soaps, oils, and even toothpastes. It’s also become a popular ingredient for facial masks.</p>
+		<p>Although there is limited research into the skin benefits of activated charcoal, some skin care experts believe a charcoal mask can help your skin in the following ways:</p>
+	</div>
+	<div class="row">
+		
+		<ul>
+			<li><b>Removes impurities</b><br>
+				<p>Because research has shown that activated charcoal can absorb toxins in your body, some beauty experts believe that a charcoal face mask can help draw impurities and dirt from your skin.</p>
+			</li>
+			<li><b>Reduces acne breakouts</b><br>
+				<p>An accumulation of sebum (skin oils) and bacteria can clog your pores, resulting in breakouts. If you’re looking for a natural acne remedy, activated charcoal may help by removing bacteria and other impurities from your pores.</p>
+			</li>
+			<li><b>Controls oiliness</b><br>
+				<p>By removing dead skin cells and absorbing excess oil, activated charcoal may help give your skin a healthy glow without too much shine.</p>
+			</li>
+		</ul>
+	</div>
+</div>
+
+<?php
+include_once 'foot.php';
+?>
